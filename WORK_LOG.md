ee06451c909894949427f4f8bc4220c1d7574b4a
kate <kate.little@menloschool.org>
2024-09-02 19:15:48 -0700

    9/2 - recursive pseudocode & quicksort draft

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,11 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date  |   Time    |                                                                                                                                                        Update |
+|:------|:---------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Sep 1 | 7-7:30pm  |                                                                              I considered how to use a recursive/DFS approach to find all possible # of moves |
+| Sep 2 | 3:00-3:30 | I decided my previous approach would be inefficient, so I pivoted to checking for the longest ladder & building my approach that way. I wrote new pseudocode. |
+| Sep 2 | 3:30-4:30 |  I realized my new idea would not account for all possibilities, so I switched back to a recursive approach. I drafted a quicksort method & wrote pseudocode. |
+|       |           |                                                                                                                                                               |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -15,7 +18,77 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        System.out.println("boardsize: " + boardsize);
+        System.out.println(Arrays.deepToString(ladders));
+        System.out.println(Arrays.deepToString(snakes));
 
+        int moves = 0;
+        // Sort ladders by starting position to make traversing easier
+        // Call quicksort method
+        quickSort(ladders);
+        System.out.println(Arrays.deepToString(ladders));
+
+        // Use recursion to consider all possible combos of ladders.
+
+        // For each combo, add # of moves to reach start of ladders, # of moves b/w ladders, and # of moves to reach end
+        // Store all of these counts in an arraylist
+        // Return lowest value in arraylist
         return 0;
     }
+
+    // Reference code: https://www.geeksforgeeks.org/quick-sort-algorithm/
+    // Quicksort used to organize ladders
+    public static void quickSort(int[][] ladders){
+        // Find low & high start values for quicksort
+        int low = 100;
+        int high = 1;
+        for (int[] ladder: ladders){
+            if (ladder[0] > high) {
+                high = ladder[0];
+            }
+            if (ladder[0] < low) {
+                low = ladder[0];
+            }
+        }
+        quickSortHelper(ladders, low, high);
+    }
+
+    // Change return type
+    // Quicksort helper method
+    public static void quickSortHelper(int[][] ladders, int low, int high){
+        if (low < high){
+            // Get pivot index of array
+            int pivot = partition(ladders, low, high);
+            // Sort ladders by start value on both sides of the pivot
+            // Recursion!
+            quickSortHelper(ladders, low, pivot - 1);
+            quickSortHelper(ladders, pivot + 1, high);
+        }
+    }
+
+    // Put all smaller values on left of pivot & bigger values on right
+    public static int partition(int[][] ladders, int low, int high){
+        // Pivot = last element's start value
+        int pivot = ladders[high][0];
+        // Index of element smaller than pivot
+        int i = low - 1;
+
+        for (int j = low; j < high; j++){
+            // If current element's start value is smaller than or equal to pivot's start value
+            if (ladders[j][0] <= pivot){
+                // Swap smaller & larger elements
+                int[] temp = ladders[i];
+                ladders[i] = ladders[j];
+                ladders[j] = temp;
+            }
+        }
+
+        // Swap elements into proper positions
+        int[] temp2 = ladders[i + 1];
+        ladders[i + 1] = ladders[high];
+        ladders[high] = temp2;
+
+        return i + 1;
+    }
 }
+
